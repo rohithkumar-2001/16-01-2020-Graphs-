07f844314168d0fa1cc530e4cd0e169167837b25
rohithkumar-2001 <61039670+rohithkumar-2001@users.noreply.github.com>
2021-01-16 12:42:46 +0530

    Create BFS.java

---
+++ BFS.java
@@ -0,0 +1,60 @@
+// Initial Template for Java
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while(T-->0)
+        {
+            String[] s = br.readLine().trim().split(" ");
+            int V = Integer.parseInt(s[0]);
+            int E = Integer.parseInt(s[1]);
+            ArrayList<ArrayList<Integer>>adj = new ArrayList<>();
+            for(int i = 0; i < V; i++)
+                adj.add(i, new ArrayList<Integer>());
+            for(int i = 0; i < E; i++){
+                String[] S = br.readLine().trim().split(" ");
+                int u = Integer.parseInt(S[0]);
+                int v = Integer.parseInt(S[1]);
+                adj.get(u).add(v);
+                // adj.get(v).add(u);
+            }
+            Solution obj = new Solution();
+            ArrayList<Integer>ans = obj.bfsOfGraph(V, adj);
+            for (int i =0 ;i < ans.size (); i++) 
+                System.out.print (ans.get (i) + " ");
+            System.out.println();
+        }
+    }
+}
+// } Driver Code Ends
+
+
+class Solution
+{
+    public ArrayList<Integer> bfsOfGraph(int V, ArrayList<ArrayList<Integer>> adj)
+    {
+        boolean prev[]=new boolean[V];
+        Queue<Integer> pres=new LinkedList<>();
+        ArrayList<Integer> result=new ArrayList<>();
+        prev[0]=true;
+        pres.add(0);
+        while(!pres.isEmpty()){
+            int temp=pres.poll();
+            result.add(temp);
+            Iterator<Integer> it=adj.get(temp).iterator();
+            while(it.hasNext()){
+                int n=it.next();
+                if(!prev[n]){
+                    prev[n]=true;
+                    pres.add(n);
+                }
+            }
+        }
+        return result;
+    }
+}
